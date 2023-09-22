304c93795149bfb688d2b40bb09814f998680f42
gavinarori <99688077+gavinarori@users.noreply.github.com>
2023-09-22 09:32:59 +0300

    first commit

---
+++ Dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+let id = 7;
+let company = "employees";
+let age = 23;
+let x;
+let isTrue = true;
+let ids = [15, 3];
+let people;
+ids.push(7);
+console.log("ids:", ids);
+//tuple
+const product = [1, 'rtgc', true, 3];
+//union
+let h;
+h = true;
+//Enumerator
+var direction;
+(function (direction) {
+    direction[direction["up"] = 0] = "up";
+    direction[direction["down"] = 1] = "down";
+    direction[direction["left"] = 2] = "left";
+    direction[direction["right"] = 3] = "right";
+})(direction || (direction = {}));
+console.log(direction.up);
---
+++ src/index.ts
@@ -0,0 +1,40 @@
+let id:number = 7;
+let company:string = "employees"
+let age:number = 23
+let x:any 
+let isTrue:boolean = true
+let ids:number[]=[15,3]
+let people:string | boolean[]
+
+ids.push(7);
+console.log("ids:",ids)
+
+//tuple
+const product:[number,string,boolean,any]= [1,'rtgc',true,3]
+
+//union
+let h:number | boolean
+h=true 
+
+//Enumerator
+enum direction {
+  up,
+  down,
+  left,
+  right
+}
+
+console.log(direction.up)
+
+type User = {
+    id:number,
+    name:string
+}
+
+const user:User={
+    id:1,
+    name:'peter'
+}
+
+//type assersion
+
